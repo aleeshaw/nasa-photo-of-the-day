b34885e42e0cff15b5f57f765b368e007f7300f9
aleesha-wood <spectrat@spectratart.com>
2019-09-26 21:19:04 -0400

    added styling

--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import '../ImgCard.css';
 
 const ImgCard = (props) => {
   return (
     <div className="info-card">
       <img className="card-img" src={props.url} alt="NASA pic of the day" />
-      <div className="text-info">
-        <h1 className="title">{props.title}</h1>
-        <h3 className="date">{props.date}</h3>
+      <section className="text-info">
+        <div className="title-date">
+          <h1 className="title">{props.title}</h1>
+          <h3 className="date">{props.date}</h3>
+        </div>
         <p className="info">{props.info}</p>
-      </div>   
+      </section>   
     </div>
   )
 };
